871b233e979832026b8110b085d23bb07175b400
Bill_Ji <superice_gy@163.com>
2017-07-11 01:25:13 +0800

    autosizeinput done

--- src/component/AutosizeInput.js
+++ src/component/AutosizeInput.js
@@ -12,7 +12,7 @@ const sizerStyle = {
     visibility: 'hidden',
     height: 0,
     overflow: 'scroll',
-    writeSpace: 'pre'
+    whiteSpace: 'pre'
 };
 
 class AutosizeInput extends Component {
@@ -49,6 +49,7 @@ class AutosizeInput extends Component {
         inputClassName: PropTypes.string,           // input 添加 classnames
 
         onChange: PropTypes.func,                   // input onChange 句柄
+        onAutosize: PropTypes.func                  // did Update 触发 autoSize 句柄
 
     };
 
@@ -60,9 +61,86 @@ class AutosizeInput extends Component {
         this.sizer = el;
     }
 
+    placeHolderSizerRef(el){
+        this.placeHolderSizer = el;
+    }
+
+    componentDidMount(){
+        this.mounted = true;
+        this.copyInputStyles();
+        this.updateInputWidth();
+    }
+
+    componentWillUnmount(){
+        this.mounted = false;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if(prevState.inputWidth !== this.state.inputWidth){
+            if(typeof this.props.onAutosize === 'function'){
+                this.props.onAutosize(this.state.inputWidth);
+            }
+        }
+        this.updateInputWidth();
+    }
+
+    //挂载之后获取 input css 样式
+    copyInputStyles(){
+
+        if (!this.mounted || !window.getComputedStyle) {
+            return;
+        }
+        const inputStyle = this.input && window.getComputedStyle(this.input);
+
+        if(!inputStyle){
+            return;
+        }
+
+        let widthNode = this.sizer;
+
+        widthNode.style.fontSize = inputStyle.fontSize;
+        widthNode.style.fontFamily = inputStyle.fontFamily;
+        widthNode.style.fontWeight = inputStyle.fontWeight;
+        widthNode.style.fontStyle = inputStyle.fontStyle;
+        widthNode.style.letterSpacing = inputStyle.letterSpacing;
+        widthNode.style.textTransform = inputStyle.textTransform;
+        if(this.props.placeholder){
+            let placeholderNode = this.placeHolderSizer;
+            placeholderNode.style.fontSize = inputStyle.fontSize;
+            placeholderNode.style.fontFamily = inputStyle.fontFamily;
+            placeholderNode.style.fontWeight = inputStyle.fontWeight;
+            placeholderNode.style.fontStyle = inputStyle.fontStyle;
+            placeholderNode.style.letterSpacing = inputStyle.letterSpacing;
+            placeholderNode.style.textTransform = inputStyle.textTransform;
+        }
+    }
+
+    //获取 sizer width 并且更改 input width
+    updateInputWidth(){
+        if(!this.mounted || !this.sizer || typeof this.sizer.scrollWidth === 'undefined'){
+            return;
+        }
+        let newInputWidth = this.sizer.scrollWidth + 2;
+
+        if(this.props.placeholder && this.props.placeholderIsMinWidth){
+            newInputWidth = Math.max(newInputWidth, this.placeHolderSizer.scrollWidth) + 2;
+        }
+
+        if(newInputWidth < this.props.minWidth){
+            newInputWidth = this.props.minWidth;
+        }
+
+        if(newInputWidth !== this.state.inputWidth){
+            this.setState({
+                inputWidth: newInputWidth
+            });
+        }
+    }
+
     render(){
 
-        const {className, style, defaultValue, value, inputClassName} = this.props;
+        const {className, style, defaultValue, value, inputClassName, placeholder} = this.props;
         const sizerValue = [defaultValue, value, ''].reduce((prev, curr)=>{
             if(prev !== undefined && prev !== null) return prev;
             return curr;
@@ -98,7 +176,7 @@ class AutosizeInput extends Component {
                 <input {...inputProps} ref={this.inputRef.bind(this)}/>
                 <div ref={this.sizerRef.bind(this)} style={sizerStyle}>{sizerValue}</div>
                 {
-
+                    placeholder ? <div ref={this.placeHolderSizerRef.bind(this)} style={sizerStyle}>{placeholder}</div> : null
                 }
             </div>
         );
--- src/entry.js
+++ src/entry.js
@@ -4,10 +4,11 @@ import React from 'react';
 import {render} from 'react-dom';
 //import SetStateTest from './component/SetStateTest.js';
 //import AnimationTest from '../test/AnimationTest2.js';
-import AutosizeInput from './component/AutosizeInput.js';
+//import AutosizeInput from './component/AutosizeInput.js';
+//import TestAutoSizeInput from '../test/testAutoSizeInput.js';
 
 import App from './component/App.js';
 
 
-render(<AutosizeInput />, document.querySelector('#App'));
+render(<App/>, document.querySelector('#App'));
 
